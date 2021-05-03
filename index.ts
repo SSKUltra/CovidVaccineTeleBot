2d8c9fa92533ad9cb80d1c5032d58b7c8825e51f
Sarvesh <sarvesh27k@gmail.com>
2021-05-03 20:14:50 +0530

    Add text formatting and 429

--- index.ts
+++ index.ts
@@ -7,11 +7,21 @@ const bot = new Telegraf(process.env.TOKEN);
 const host = "https://cdn-api.co-vin.in/api/v2";
 
 const emoji = {
-    Cash: '\u{1F4B8}',
+    Cash: '\u{1F4B5}',
+    Free: '\u{1F193}',
+    Pup: '\u{1F436}',
+    Clock: '\u{1F553}',
+    Check: '\u{2705}',
+    Cross: '\u{274C}',
 }
 
+const userFileName = 'users.json';
 const districtFileName = 'districts.json';
 const districtDataFileName = 'districtsData.json';
+
+const POLLING_INTERVAL = 30000;
+const BUCKET_SIZE = 5;
+const BACKOFF_MULTIPLIER = 2000;
 
 const botCommandList = [
     {
@@ -21,11 +31,17 @@ const botCommandList = [
     {
         command: 'reset',
         description: 'Remove all registered districts.'
+    },
+    {
+        command: 'stop',
+        description: 'Stop receiving updates from me.'
     }
 ]
 
-bot.start((ctx) => {
+bot.start((ctx) => { 
+    bot.telegram.sendMessage(ctx.chat.id, `Hello ${ctx.chat.first_name} welcome to CovidIndiaVaccineBot, I will send you details about current available vaccination slots in your area and notify you when there are changes in the availability of those slots.`);
     fetchState(ctx);
+    jsonAddUserData(ctx.from.id, ctx.chat.first_name);
 })
 
 bot.telegram.setMyCommands(botCommandList);
@@ -35,7 +51,21 @@ bot.hears('/add', (ctx) => {
 })
 
 bot.hears('/reset', (ctx) => {
+    bot.telegram.sendMessage(ctx.chat.id, 'Are you sure you want to remove all your preferences?', requestConfirmation)
+})
+
+bot.hears('/stop', (ctx) => {
+    bot.telegram.sendMessage(ctx.chat.id, 'Are you sure you want to stop receiving updates from me?', requestConfirmation)
+})
+
+bot.action('YesResetConfirmation', (ctx) => {
     removeUserFromAllDistricts(ctx.from.id);
+    bot.telegram.sendMessage(ctx.chat.id, `You have unsubscribed. I hope I was helpful. ${emoji.Pup}`);
+    bot.telegram.editMessageReplyMarkup(ctx.chat.id, ctx.update.callback_query.message.message_id, undefined, {"reply_markup": null})
+})
+
+bot.action('NoResetConfirmation', (ctx) => {
+    bot.telegram.editMessageReplyMarkup(ctx.chat.id, ctx.update.callback_query.message.message_id, undefined, {"reply_markup": null})
 })
 
 bot.use(session())
@@ -78,6 +108,23 @@ const requestAgeGroup = {
         ]
     }
 };
+
+const requestConfirmation = {
+    "reply_markup": {
+        "inline_keyboard": [
+            [
+                {
+                    "text": 'Yes',
+                    "callback_data": 'YesResetConfirmation',
+                },
+                {
+                    "text": 'No',
+                    "callback_data": 'NoResetConfirmation',
+                }
+            ]
+        ]
+    }
+}
 
 const generateUserData = (userName, districtId, ageGroup) => {
     return districtId ? 
@@ -150,7 +197,7 @@ const removeKeyboard = {
 }
 
 const replyParseMode = {
-    "parse_mode": "MarkdownV2"
+    "parse_mode": "HTML"
 }
 
 const getCurrentDate = () => {
@@ -162,16 +209,16 @@ const getCurrentDate = () => {
 }
 
 const generateCenterMessage = (centerData) => 
-    `\`Name : ${centerData.name} \n` +
+    `<b>Name : ${centerData.name}</b> \n` +
     `Minimum age : ${centerData.min_age_limit} \n` +
-    `Available capacity : ${centerData.available_capacity} \n` + 
+    `<b>Available capacity : ${centerData.available_capacity}</b> ${centerData.available_capacity > 0 ? emoji.Check : emoji.Cross} \n` + 
     `Block name : ${centerData.block_name} \n` +
     `Pin code : ${centerData.pincode} \n` +
     `Fee type : ${centerData.fee_type} \n` + 
-    `Fee : ${centerData.fee} ${emoji.Cash}\n` + 
+    `Fee : ${centerData.fee} ${centerData.fee > 0 ? emoji.Cash : emoji.Free}\n` + 
     `Vaccine type : ${centerData.vaccine} \n` + 
     `Date : ${centerData.date} \n` +
-    `Slots : ${centerData.slots.map((slot) => `\n\t -> ${slot}`)} \n\``
+    `Slots : <code>${centerData.slots.map((slot) => `\n\t ${emoji.Clock} ${slot}`)}</code> \n`
 
 const getDistrictDataFromFile = () => {
     const districtsFromFile = fs.readFileSync(districtDataFileName, { encoding: 'utf8' });
@@ -184,22 +231,23 @@ const getDistrictDataByIdFromFile = (districtId) => {
 }
 
 const sendDistrictDataToUserByAge = (ctx, districtData, ageGroup) => {
-    districtData.sessions.map((session) => {
+    districtData.sessions.map((session, idx) => {
         if ((ageGroup === "upperAge" && session.min_age_limit === 45) || 
             (ageGroup === "lowerAge" && session.min_age_limit === 18) || 
             (ageGroup === "bothAge")) {
-            bot.telegram.sendMessage(
+            setTimeout(() => bot.telegram.sendMessage(
                 ctx.chat.id,
                 generateCenterMessage(session),
                 replyParseMode
-            ) 
+            ), Math.floor(idx/BUCKET_SIZE) * BACKOFF_MULTIPLIER)
+             
         }
     })
 
     if (districtData.sessions.length === 0 || 
         (ageGroup === "upperAge" && !districtData.sessions.find((session) => session.min_age_limit === 45)) ||
         (ageGroup === "lowerAge" && !districtData.sessions.find((session) => session.min_age_limit === 18))) {
-        bot.telegram.sendMessage(ctx.chat.id, "There was no vaccination centers available for the parameters selected by you. I will update you once there are any changes in the availability (This bot is still in testing, please don't rely only on this bot for updates).");
+        bot.telegram.sendMessage(ctx.chat.id, "There were no vaccination centers available for the parameters selected by you. I will update you once there are any changes in the availability (This bot is still in testing, please don't rely only on this bot for updates).");
     }
 }
 
@@ -234,6 +282,21 @@ const fetchDistrictData = async (ctx, ageGroup, districtId) => {
     }
 }
 
+const jsonAddUserData = (userId, userName) => {
+    try {
+        const fileUserData = fs.readFileSync(userFileName, { encoding: 'utf8' });
+        const userData = JSON.parse(fileUserData);
+
+        if (!(userId in userData)) {
+            const newUserData = { ...userData, [userId]: userName }
+            fs.writeFileSync(userFileName, JSON.stringify(newUserData));
+        }
+
+    } catch (e) {
+        console.log(e);
+    }
+}
+
 const jsonAddDistrict = (ctx, ageGroup) => {
     try {
         const fileDistrictDataResponse = fs.readFileSync(districtFileName, { encoding: 'utf8' });
@@ -256,6 +319,8 @@ const jsonAddDistrict = (ctx, ageGroup) => {
 
         console.log(`Adding user : ${userId} to district : ${districtId}`)
         fs.writeFileSync(districtFileName, JSON.stringify(newDistrictData));
+           
+        bot.telegram.sendMessage(ctx.chat.id, 'Thank you for adding your preferences. I will notify you when a vaccination slot is available in your area.');
         fetchDistrictData(ctx, ageGroup, districtId);
     }
     catch (err) {
@@ -282,8 +347,12 @@ const checkForUpdates = (districtId, oldData, newData) => {
     newData.sessions.forEach((newCenter) => {
         const { center_id } = newCenter;
         const oldCenter = oldData.sessions.find((oldCenter) => oldCenter.center_id === center_id)
-        if ((oldCenter && (oldCenter.available_capacity !== newCenter.available_capacity || oldCenter.min_age_limit !== newCenter.min_age_limit)) || 
-            !oldCenter) {
+        if ((oldCenter && (
+                (oldCenter.available_capacity === 0 && newCenter.available_capacity > 0) || 
+                oldCenter.min_age_limit !== newCenter.min_age_limit || 
+                oldCenter.date !== newCenter.date
+            )) || !oldCenter
+        ) {
             updateData = true;
             if (districtUserData) {
                 let usersToUpdate = [];
@@ -295,9 +364,11 @@ const checkForUpdates = (districtId, oldData, newData) => {
                     districtUserData["upperAge"].forEach((user) => usersToUpdate.push(user))
                 }
 
-                usersToUpdate.forEach((userId) => {
-                    console.log(`Sending message to user : ${userId} for center : ${center_id}`)
-                    bot.telegram.sendMessage(userId, generateCenterMessage(newCenter), replyParseMode)
+                usersToUpdate.forEach((userId, idx) => {
+                    setTimeout(() => {
+                        console.log(`Sending message to user : ${userId} for center : ${center_id}`)
+                        bot.telegram.sendMessage(userId, generateCenterMessage(newCenter), replyParseMode)
+                    }, Math.floor(idx/BUCKET_SIZE) * BACKOFF_MULTIPLIER) 
                 })
             }
         }
@@ -311,8 +382,7 @@ const getAllDistrictData = () => {
 
     Object.keys(districtData).forEach(async (districtId) => {
         const todayDateFormat = getCurrentDate();
-        try {
-            await axios.get(`${host}/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${todayDateFormat}`)
+        axios.get(`${host}/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${todayDateFormat}`)
             .then((response) => {
                 const updateData = checkForUpdates(districtId, districtData[districtId], response.data)
                 if (updateData) {
@@ -320,14 +390,9 @@ const getAllDistrictData = () => {
                     setDistrictDataByIdToFile(districtId, response.data);
                 }
             })
-        } catch (e) {
-            console.log(e);
-        }
-        
+            .catch((e) => console.log(e))
     })
 }
-
-const POLLING_INTERVAL = 5000;
 
 const poll = async ({ fn, interval }) => {  
     const executePoll = async (resolve, reject) => {
@@ -335,7 +400,7 @@ const poll = async ({ fn, interval }) => {
         const result = await fn();
         setTimeout(executePoll, interval, resolve, reject);
     }
-  
+
     return new Promise(executePoll);
 };
 
